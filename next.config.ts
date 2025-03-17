2ff33e18424b2021d3cc3849cd26b438917db9ac
YAMADANI <yamagamihayato@outlook.com>
2025-03-17 19:02:33 +0900

    デプロイの調整

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/yoogletodo",
-  assetPrefix: "/yoogletodo",
+  assetPrefix: "/yoogletodo/",
 };
 
 export default nextConfig;
