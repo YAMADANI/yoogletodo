edbec17d7db074b569ccf61bdcdd7261cb1d5896
YAMADANI <yamagamihayato@outlook.com>
2025-03-17 18:09:39 +0900

    READMEの編集

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/リポジトリ名",
+  assetPrefix: "/リポジトリ名/",
 };
 
 export default nextConfig;
